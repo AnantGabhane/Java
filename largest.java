5234cb4e0548d9b2467ed78b4f6e7d1176c22261
AnantGabhane <gabhaneanant90@gmail.com>
2023-02-16 00:01:50 +0530

    Largest Number

---
+++ largest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class largest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // input 3 numbers
+        // int a = in.nextInt();
+        // int b = in.nextInt();
+        // int c = in.nextInt();
+
+        // //Q. find largeest of all
+        // int max =a;
+        // if(b>max){
+        //     max =b;
+        // }
+        // if(c>max){
+        //     max=c;
+        // }
+        // System.out.println(max);
+        //using function
+        // int max=Math.max(c, Math.max(a,b));
+        System.out.println(Math.max(34,37));
+
+    }
+}
