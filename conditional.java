6b901d03a6108bd5d924ceb62f89edfd42e55080
AnantGabhane <gabhaneanant90@gmail.com>
2023-02-15 23:50:50 +0530

    For condition

--- conditional.java
+++ conditional.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class conditional {
     public static void main(String[] args) {
-        int salary = 25000;
-        if(salary > 10000){
-            salary +=2000;
-        }
-        else{
-            salary +=1000;
-        }
-        System.out.println(salary);
+    //     int salary = 25000;
+    //     if(salary > 10000){
+    //         salary +=2000;
+    //     }
+    //     else{
+    //         salary +=1000;
+    //     }
+    //     System.out.println(salary);
+    
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+
+    for(int num = 1; num < n; num++){
+        System.out.print(num +" ");
+    }
     }
 }
