cf1b99332b705312d28d3a762fa9b14e7eebb7f9
AnantGabhane <gabhaneanant90@gmail.com>
2023-02-18 11:00:41 +0530

    New Commit

---
+++ Casecheak.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Casecheak {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        char ch = in.next().trim().charAt(0);
+        System.out.println(ch);
+        // first word at index zero is taken out
+        // String word ="hello";
+        // System.out.println(word.charAt(0));
+    }
+}
