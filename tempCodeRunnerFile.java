1e4cf72a0d24e850b9b250e78d46040f6a7c39cb
Anant Gabhane <gabhaneanant90@gmail.com>
2023-02-23 00:15:43 +0530

    Checks Case of Input variable

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public Class Casecheak {
+public class casecheak {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); 
         char ch = in.next().trim().charAt(0);
