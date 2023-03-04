e323224eed1de8f57d5761ff15a4341bde28e529
Anant Gabhane <gabhaneanant90@gmail.com>
2023-03-04 16:48:03 +0530

    Update in casecheck

--- Casecheak.java
+++ Casecheak.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
-public Class Casecheak {
+public class Casecheak {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); 
         char ch = in.next().trim().charAt(0);
-        System.out.println(ch);
+        //both should be true for cond to be true
+        if(ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase " + ch);
+        }else{
+            System.out.println("Uppercase " + ch);
+        }
+        
+        
+        
+        
+       // System.out.println(ch);
+
+
 
         
-        // first word at index zero is taken out
+        //first word at index zero is taken out
         // String word ="hello";
-        // System.out.println(word.charAt(0));
+        // System.out.println(word.charAt(2));
     }
 }
