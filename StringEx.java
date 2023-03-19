71059941f8c0dad84f5b93ae1fc5a0603d830078
Anant Gabhane <gabhaneanant90@gmail.com>
2023-03-20 00:51:39 +0530

    String examole to understand functions

---
+++ StringEx.java
@@ -0,0 +1,11 @@
+public class StringEx {
+    public static void main(String[] args) {
+       String message =  greet();
+       System.out.println(message);
+    }
+
+    static String greet(){
+        String greeting = "hey how are you??";
+        return greeting;
+    }
+}
