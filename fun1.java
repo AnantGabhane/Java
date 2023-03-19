e7e82b20120305d531486a214e01bd784134c38b
Anant Gabhane <gabhaneanant90@gmail.com>
2023-03-20 00:59:43 +0530

    Function with arguments

--- fun1.java
+++ fun1.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class fun1 {
     public static void main(String[] args) {
+        // int ans = sum2();
+        // System.out.println(ans);
+        int ans = sum3(20, 30);
+        System.out.println(ans);
+    }
+
+    //passing arguments
+    static int sum3(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter num1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter num2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
         
     }
 }
