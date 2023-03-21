c0cdb9a3ba3f54a297c511ed3802c2f3f3735b56
Anant Gabhane <gabhaneanant90@gmail.com>
2023-03-21 20:04:59 +0530

    Swapping and Reverse of array

--- Arrays-Java/Array/Swap.java
+++ Arrays-Java/Array/Swap.java
@@ -2,9 +2,23 @@ import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
         int[] arr = {1,3,33,4,5,6};
-        swap(arr, 1, 3);
-        System.out.println(Arrays.toString(arr));
+       // swap(arr, 1, 3);
+       Reverse(arr); 
+       System.out.println(Arrays.toString(arr));
     }
+
+    static void Reverse(int[] arr){
+        int start =0;
+        int end = arr.length-1;
+        while(start < end){
+            //swapping of elements
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+
     static void swap(int[] arr, int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
