ab23b8f9c78e17df8985b0ec7fb4e9e9221a2ba9
Anant Gabhane <gabhaneanant90@gmail.com>
2023-04-10 16:32:16 +0530

    all

---
+++ Binary-Search/findDisapperNumber.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class findDisapperNumber {
+    public static void main(String[] args) {
+        int[] nums = {};
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // just find the missing number
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
