ca2dcf85c6caae331bddb22b93c51e37e142dffe
Anant Gabhane <gabhaneanant90@gmail.com>
2023-04-13 00:26:45 +0530

    String practice

--- Strings/src/New.java
+++ Strings/src/New.java
@@ -1,3 +1,14 @@
 public class New {
-
+    public static void main(String[] args) {
+        String a = new String("Anant");
+        String b = new String("Anant");
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        System.out.println(a.charAt(0));
+    }
 }
+
+// op
+// false
+// true
+// A
