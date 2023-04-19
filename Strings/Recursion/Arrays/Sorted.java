afac45bca964ffcc1a898350e2c5b01640acfe80
AnantGabhane <gabhaneanant90@gmail.com>
2023-04-19 23:46:17 +0530

    check for sorted array

---
+++ Strings/Recursion/Arrays/Sorted.java
@@ -0,0 +1,16 @@
+public class Sorted {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 5, 6, 8, 9 };
+        System.out.println(isSorted(arr, 0));
+    }
+
+    static boolean isSorted(int[] arr, int index) {
+        // base condition
+        if (index == arr.length - 1) {
+            return true;
+        }
+
+        return arr[index] < arr[index + 1] && isSorted(arr, index + 1);
+
+    }
+}
