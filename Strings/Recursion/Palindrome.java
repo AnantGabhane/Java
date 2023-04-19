6dcb7b08864dc284915cff9d0d35d00917620a51
AnantGabhane <gabhaneanant90@gmail.com>
2023-04-19 14:33:37 +0530

    checks for palindrome

---
+++ Strings/Recursion/Palindrome.java
@@ -0,0 +1,26 @@
+public class Palindrome {
+    static int rev(int n) {
+        // somrtimes you may need to have variable base in the arguments
+        // then make another function, called helper() function
+        int digits = (int) (Math.log10(n) + 1);
+        return helper(n, digits);
+    }
+
+    static int helper(int n, int digits) {
+        if (n % 10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return (int) (rem * Math.pow(10, digits - 1) + helper(n / 10, digits - 1));
+    }
+
+    static boolean palin(int n) {
+        return n == rev(n);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(palin(134321));
+    }
+
+}
