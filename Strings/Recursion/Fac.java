73a6621dd678b4ed03a7245dfc51f3c0a6c0fffc
AnantGabhane <gabhaneanant90@gmail.com>
2023-04-19 13:04:49 +0530

    recursion in factorial

---
+++ Strings/Recursion/Fac.java
@@ -0,0 +1,10 @@
+public class Fac {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    static int factorial(int n) {
+        return n == 0 ? 1 : n * factorial(n - 1);
+    }
+
+}
