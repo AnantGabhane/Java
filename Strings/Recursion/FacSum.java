f68bdd79da1f364bcae1a81a14fb805fe0e4ad17
AnantGabhane <gabhaneanant90@gmail.com>
2023-04-19 13:16:55 +0530

    Sumof n nums

---
+++ Strings/Recursion/FacSum.java
@@ -0,0 +1,10 @@
+public class FacSum {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    static int factorial(int n) {
+        return n <= 1 ? 1 : n + factorial(n - 1);
+    }
+
+}
