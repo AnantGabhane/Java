ae5592d7b4bcd3c54ee5b2913ab805a15a8bbe36
AnantGabhane <gabhaneanant90@gmail.com>
2023-04-19 23:50:26 +0530

    check for sorted array

--- Strings/Recursion/Arrays/Find.java
+++ Strings/Recursion/Arrays/Find.java
@@ -1,5 +1,16 @@
 package Arrays;
 
 public class Find {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean find(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return false;
+        }
+        return arr[index] == target || find(arr, target, index + 1);
+    }
+    // return the index value
     
 }
