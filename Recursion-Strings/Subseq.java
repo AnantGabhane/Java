db2f91f9a05a84a8c146d593ed73b51593da9f95
AnantGabhane <gabhaneanant90@gmail.com>
2023-04-26 15:28:30 +0530

    prints all subset

--- Recursion-Strings/Subseq.java
+++ Recursion-Strings/Subseq.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class Subseq {
     public static void main(String[] args) {
-        System.out.println(Asubseq("", "abc"));
+        // subseqASCII("", "abc");
+        System.out.println(AsubseqASCII("", "abc"));
     }
 
     static void subseq(String p, String up) {
@@ -30,4 +31,36 @@ public class Subseq {
         return left;
 
     }
+
+    // using ascii value of that particular string
+    static void subseqASCII(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseqASCII(p + ch, up.substring(1));
+        subseqASCII(p, up.substring(1));
+        subseqASCII(p + (ch + 0), up.substring(1));
+    }
+
+    // save it in an array list
+    static ArrayList<String> AsubseqASCII(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> first = AsubseqASCII(p + ch, up.substring(1));
+        ArrayList<String> second = AsubseqASCII(p, up.substring(1));
+        ArrayList<String> third = AsubseqASCII(p + (ch + 0), up.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+
+        return first;
+
+    }
+
 }
