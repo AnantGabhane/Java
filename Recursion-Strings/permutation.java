1d271207d45b306c460ca1471d4c38a09bfd213f
AnantGabhane <gabhaneanant90@gmail.com>
2023-04-26 18:36:37 +0530

    prints no of permutations of str andfunction calls

--- Recursion-Strings/permutation.java
+++ Recursion-Strings/permutation.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 public class permutation {
     public static void main(String[] args) {
-        Permutations("", "abc");
+        // Permutations("", "abc");
+
+        // ArrayList<String> ans = PermutationsList("", "abc");
+        // System.out.println(ans);
+        System.out.println(PermutationsCount("", "abcd"));
     }
 
     static void Permutations(String p, String up) {
@@ -15,4 +21,36 @@ public class permutation {
             Permutations(f + ch + s, up.substring(1));
         }
     }
+
+    static ArrayList<String> PermutationsList(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(PermutationsList(f + ch + s, up.substring(1)));
+        }
+        return ans;
+    }
+
+    static int PermutationsCount(String p, String up) {
+        if (up.isEmpty()) {
+            // System.out.println(p);
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            count = count + PermutationsCount(f + ch + s, up.substring(1));
+        }
+        return count;
+    }
+
 }
